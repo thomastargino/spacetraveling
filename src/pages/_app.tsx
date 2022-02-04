394dc27fef4361f5495d4f06f877cddd3c56fada
Thomás Targino <targinothomas@gmail.com>
2022-02-04 08:51:55 -0300

    Add progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNProgress color="#FF57B2" />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
